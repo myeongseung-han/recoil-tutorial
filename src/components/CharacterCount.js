c29f9eab1c908571c1a3577ef93c0716700896ca
myeongseung-han <hms3512@gmail.com>
2022-07-22 20:26:03 +0900

    [feat] CharacterCount 컴포넌트 추가

---
+++ src/components/CharacterCount.js
@@ -0,0 +1,11 @@
+import React from "react";
+import {useRecoilValue} from "recoil";
+import { charCountState } from "../recoil/selectors";
+
+function CharacterCount() {
+    const count = useRecoilValue(charCountState);
+  
+    return <>Character Count: {count}</>;
+  }
+
+export default CharacterCount;
